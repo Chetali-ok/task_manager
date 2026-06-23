162f14defbe660efdf0fba6aff6c267b87a34f11
chetali <chetali@i2ifunding.co.in>
2026-06-23 22:45:53 +0530

    signup UI

--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
-
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
+
+  username = '';
+  password = '';
+  showPassword = false;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}  
+
+    signup(){
+    console.log('SIGNUP BUTTON CLICKED');
+
+    const data ={
+      username: this.username,
+      password: this.password,
+    };
+
+    this.authService.signup(data).subscribe({
+      next: (response:any) => {
+        localStorage.setItem('token', response.access);
+        console.log(response);
+
+        this.router.navigate(['/dashboard']);
+      },
+      error: (error) =>{
+        console.log(error);
+      }
+    });
+  }
 
 }
